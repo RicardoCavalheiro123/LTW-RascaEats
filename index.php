32b2c237f0883b9c7b1a78759bb46bcaf1baf09f
FranciscoMaldonado0 <up202004244@up.pt>
2022-05-10 14:23:38 +0100

    Restaurant page beginning

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/7dd8778261.js" crossorigin="anonymous"></script>
-    <title>Ex</title>
+    <title>Rasca Eats</title>
 </head>
 <body>
 <header>
---
+++ restaurant.php
@@ -0,0 +1,86 @@
+<?php 
+
+    require_once('sql/connection.php');
+    require_once('sql/restaurant.php');
+
+    $db = getDatabaseConnection();
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $categories = getCategories($db);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="restaurant.css">
+    <script src="https://kit.fontawesome.com/7dd8778261.js" crossorigin="anonymous"></script>
+    <title>Restaurante</title>
+</head>
+<body>
+<header>
+        <h1>Rasca Eats</h1>
+        <i class="fa-solid fa-utensils"></i>
+        <form action="https://www.google.pt/?hl=pt-PT" method="get" id="loginForm">
+            <div class="login">
+                <input type="button" value="Login | Register">
+            </div>
+        </form>
+        <form action="file:///C:/Users/antol/LTW_php/Projeto_LTW/proj.html" method="get">
+            <div class="search">
+                <input type="text" class="searchInput" name="search" placeholder="search...">
+                <button type="submit" class="searchButton">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
+        </form>
+            
+    </header>
+    
+
+    <section id= "restaurant">
+        <h2>
+            Category
+        </h2>
+        <h2>
+            Nome
+        </h2>
+        <h2>
+            Rating
+        </h2>
+        <h2>
+            Morada <br>
+            Contacto
+        </h2>
+        <img src="https://picsum.photos/300/300?food1" alt="Restaurant photo">
+    </section>
+    <section id = "reviews"> 
+            Deixe o seu comentário
+            <article>
+                <h3>abc disse: </h3>
+                <h4>Gostei muito</h4>
+            </article>
+            <article>
+                <h3>abc disse: </h3>
+                <h4>Gostei muito</h4>
+            </article>
+            <article>
+                <h3>abc disse: </h3>
+                <h4>Gostei muito</h4>
+            </article>
+        </section>
+    <footer>
+        <div class="footer-content">
+            <h3>Descubra e reserve dos melhores restaurantes</h3>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy;2022 Rasca Eats</p>
+        </div>
+    </footer>
+
+    </body>
+</html>
